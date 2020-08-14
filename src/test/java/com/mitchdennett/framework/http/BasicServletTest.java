c8bb642a63cdba01fc38420eaf3bcd9302d2672c
Mitchell Dennett <mitch@mylodevelopment.com>
2020-08-14 15:59:36 -0400

    more basic servlet tests

--- src/test/java/com/mitchdennett/framework/http/BasicServletTest.java
+++ src/test/java/com/mitchdennett/framework/http/BasicServletTest.java
@@ -27,18 +27,9 @@ public class BasicServletTest {
     public void testDoGet() throws IOException, InvocationTargetException, IllegalAccessException {
         Container c = mock(Container.class);
         Router r = mock(Router.class);
-        BasicDataSource ds = mock(BasicDataSource.class);
         HttpServletRequest req = mock(HttpServletRequest.class);
         HttpServletResponse resp = mock(HttpServletResponse.class);
-        given(c.make(BasicDataSource.class)).willReturn(ds);
-        given(req.getMethod()).willReturn("GET");
-        given(req.getRequestURI()).willReturn("/");
-        Route route = mock(Route.class);
-        given(r.lookup(any(String.class), any(HttpMethod.class), any(Request.class))).willReturn(route);
-        given(route.getMiddlewareBefore()).willReturn(new ArrayList<MiddlewareMapper>());
-
-        BufferedReader reader = mock(BufferedReader.class);
-        given(req.getReader()).willReturn(reader);
+        setUpRequest(c, r, req, "GET", new ArrayList<>());
 
         BasicServlet servlet = new BasicServlet(c, r);
         servlet.doGet(req, resp);
@@ -59,17 +50,9 @@ public class BasicServletTest {
     public void testDoPost() throws InvocationTargetException, IllegalAccessException, IOException {
         Container c = mock(Container.class);
         Router r = mock(Router.class);
-        BasicDataSource ds = mock(BasicDataSource.class);
         HttpServletRequest req = mock(HttpServletRequest.class);
         HttpServletResponse resp = mock(HttpServletResponse.class);
-        given(c.make(BasicDataSource.class)).willReturn(ds);
-        given(req.getMethod()).willReturn("POST");
-        given(req.getRequestURI()).willReturn("/");
-        Route route = mock(Route.class);
-        given(r.lookup(any(String.class), any(HttpMethod.class), any(Request.class))).willReturn(route);
-        given(route.getMiddlewareBefore()).willReturn(new ArrayList<MiddlewareMapper>());
-        BufferedReader reader = mock(BufferedReader.class);
-        given(req.getReader()).willReturn(reader);
+        setUpRequest(c, r, req, "POST",new ArrayList<>());
 
         BasicServlet servlet = new BasicServlet(c, r);
         servlet.doPost(req, resp);
@@ -83,5 +66,58 @@ public class BasicServletTest {
 
         assertEquals("POST", reqCaptor.getValue().getMethod());
 
+    }
+
+    @Test
+    public void testDoGetWithBeforeMiddleware() throws InvocationTargetException, IllegalAccessException, IOException {
+        Container c = mock(Container.class);
+        Router r = mock(Router.class);
+        HttpServletRequest req = mock(HttpServletRequest.class);
+        HttpServletResponse resp = mock(HttpServletResponse.class);
+
+        doMiddlewareTest(c, r, req, resp, "DefaultMiddlewareBefore");
+
+        doMiddlewareTest(c, r, req, resp, "DefaultMiddlewareAfter");
+    }
+
+    private void doMiddlewareTest(Container c, Router r, HttpServletRequest req, HttpServletResponse resp, String middlewareType) throws IOException, InvocationTargetException, IllegalAccessException {
+        MiddlewareMapper mapper = mock(MiddlewareMapper.class);
+        ArrayList<MiddlewareMapper> beforeList = new ArrayList<>();
+        beforeList.add(mapper);
+
+        MiddlewareMapper mapperTwo = mock(MiddlewareMapper.class);
+        ArrayList<MiddlewareMapper> defaultList = new ArrayList<>();
+        defaultList.add(mapperTwo);
+        given(c.make(middlewareType, ArrayList.class)).willReturn(defaultList);
+
+        setUpRequest(c, r, req, "GET", beforeList);
+
+        BasicServlet servlet = new BasicServlet(c, r);
+        servlet.doPost(req, resp);
+
+        ArgumentCaptor<Container> contCaptor = ArgumentCaptor.forClass(Container.class);
+        ArgumentCaptor<Request> reqCaptor = ArgumentCaptor.forClass(Request.class);
+        ArgumentCaptor<Response> respCaptor = ArgumentCaptor.forClass(Response.class);
+
+        verify(mapper).run(contCaptor.capture(), reqCaptor.capture(), respCaptor.capture());
+
+        ArgumentCaptor<Container> contCaptor2 = ArgumentCaptor.forClass(Container.class);
+        ArgumentCaptor<Request> reqCaptor2 = ArgumentCaptor.forClass(Request.class);
+        ArgumentCaptor<Response> respCaptor2 = ArgumentCaptor.forClass(Response.class);
+
+        verify(mapperTwo).run(contCaptor2.capture(), reqCaptor2.capture(), respCaptor2.capture());
+    }
+
+    private void setUpRequest(Container c, Router r, HttpServletRequest req, String method, ArrayList<MiddlewareMapper> middleware) throws IOException {
+        BasicDataSource ds = mock(BasicDataSource.class);
+        given(c.make(BasicDataSource.class)).willReturn(ds);
+        given(req.getMethod()).willReturn(method);
+        given(req.getRequestURI()).willReturn("/");
+        Route route = mock(Route.class);
+        given(r.lookup(any(String.class), any(HttpMethod.class), any(Request.class))).willReturn(route);
+        given(route.getMiddlewareBefore()).willReturn(middleware);
+//        given(route.getMiddlewareAfter()).willReturn(middleware);
+        BufferedReader reader = mock(BufferedReader.class);
+        given(req.getReader()).willReturn(reader);
     }
 }
