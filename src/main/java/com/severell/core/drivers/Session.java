cbe0f135f6a3ed7a9cb2b62f5ec3a1bde4991e0b
Mitchell Dennett <mitch@mylodevelopment.com>
2020-09-11 17:35:09 -0400

    Commented Session

--- src/main/java/com/severell/core/drivers/Session.java
+++ src/main/java/com/severell/core/drivers/Session.java
@@ -2,9 +2,31 @@ package com.severell.core.drivers;
 
 public interface Session {
 
+    /**
+     * Get the object from the session
+     * @param key Key of the object.
+     * @return
+     */
     public Object get(String key);
+
+    /**
+     * Get the object as a String from the session
+     * @param key Key of the object.
+     * @return
+     */
     public String getString(String key);
+
+    /**
+     * Get the session id
+     * @return
+     */
     public String getId();
+
+    /**
+     * Put an object into the session
+     * @param key Key to store in the session
+     * @param value Value to be stored in the session
+     */
     public void put(String key, Object value);
 
 }
