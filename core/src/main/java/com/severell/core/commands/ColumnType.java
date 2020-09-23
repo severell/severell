2471bf9e705da6614da5b4ed9279ce2dd40abd81
Mitchell Dennett <mitch@mylodevelopment.com>
2020-09-23 12:27:36 -0400

    matched up more column types for migration

--- core/src/main/java/com/severell/core/commands/ColumnType.java
+++ core/src/main/java/com/severell/core/commands/ColumnType.java
@@ -1,11 +1,27 @@
 package com.severell.core.commands;
 
-import java.sql.Timestamp;
+import java.math.BigDecimal;
+import java.sql.*;
 
 public enum ColumnType {
-    BIT(-7),
-    TINYINT(-6),
-    SMALLINT(5),
+    BIT(-7){
+        @Override
+        public Class getJavaType() {
+            return boolean.class;
+        }
+    },
+    TINYINT(-6){
+        @Override
+        public Class getJavaType() {
+            return short.class;
+        }
+    },
+    SMALLINT(5){
+        @Override
+        public Class getJavaType() {
+            return short.class;
+        }
+    },
     INTEGER(4){
         @Override
         public Class getJavaType() {
@@ -18,12 +34,42 @@ public enum ColumnType {
             return long.class;
         }
     },
-    FLOAT(6),
-    REAL(7),
-    DOUBLE(8),
-    NUMERIC(2),
-    DECIMAL(3),
-    CHAR(1),
+    FLOAT(6){
+        @Override
+        public Class getJavaType() {
+            return double.class;
+        }
+    },
+    REAL(7){
+        @Override
+        public Class getJavaType() {
+            return float.class;
+        }
+    },
+    DOUBLE(8){
+        @Override
+        public Class getJavaType() {
+            return double.class;
+        }
+    },
+    NUMERIC(2){
+        @Override
+        public Class getJavaType() {
+            return BigDecimal.class;
+        }
+    },
+    DECIMAL(3){
+        @Override
+        public Class getJavaType() {
+            return BigDecimal.class;
+        }
+    },
+    CHAR(1) {
+        @Override
+        public Class getJavaType() {
+            return String.class;
+        }
+    },
     VARCHAR(12){
         @Override
         public Class getJavaType() {
@@ -36,23 +82,58 @@ public enum ColumnType {
             return String.class;
         }
     },
-    DATE(91),
-    TIME(92),
+    DATE(91){
+        @Override
+        public Class getJavaType() {
+            return Date.class;
+        }
+    },
+    TIME(92){
+        @Override
+        public Class getJavaType() {
+            return Time.class;
+        }
+    },
     TIMESTAMP(93){
         @Override
         public Class getJavaType() {
             return Timestamp.class;
         }
     },
-    BINARY(-2),
-    VARBINARY(-3),
-    LONGVARBINARY(-4),
+    BINARY(-2){
+        @Override
+        public Class getJavaType() {
+            return Byte[].class;
+        }
+    },
+    VARBINARY(-3){
+        @Override
+        public Class getJavaType() {
+            return Byte[].class;
+        }
+    },
+    LONGVARBINARY(-4){
+        @Override
+        public Class getJavaType() {
+            return Byte[].class;
+        }
+    },
     NULL(0),
     OTHER(1111),
     JAVA_OBJECT(2000),
     DISTINCT(2001),
-    STRUCT(2002),
-    ARRAY(2003),
+    STRUCT(2002){
+        @Override
+        public Class getJavaType() {
+            return Struct.class;
+        }
+    },
+    ARRAY(2003){
+        @Override
+        public Class getJavaType() {
+            return Object[].class;
+        }
+    },
     BLOB(2004){
         @Override
         public Class getJavaType() {
@@ -65,18 +146,43 @@ public enum ColumnType {
             return String.class;
         }
     },
-    REF(2006),
+    REF(2006){
+        @Override
+        public Class getJavaType() {
+            return Ref.class;
+        }
+    },
     DATALINK(70),
-    BOOLEAN(16),
+    BOOLEAN(16){
+        @Override
+        public Class getJavaType() {
+            return boolean.class;
+        }
+    },
     ROWID(-8),
     NCHAR(-15),
     NVARCHAR(-9),
-    LONGNVARCHAR(-16),
+    LONGNVARCHAR(-16){
+        @Override
+        public Class getJavaType() {
+            return String.class;
+        }
+    },
     NCLOB(2011),
     SQLXML(2009),
     REF_CURSOR(2012),
-    TIME_WITH_TIMEZONE(2013),
-    TIMESTAMP_WITH_TIMEZONE(2014);
+    TIME_WITH_TIMEZONE(2013){
+        @Override
+        public Class getJavaType() {
+            return Time.class;
+        }
+    },
+    TIMESTAMP_WITH_TIMEZONE(2014){
+        @Override
+        public Class getJavaType() {
+            return Timestamp.class;
+        }
+    };
 
     private int type;
 
