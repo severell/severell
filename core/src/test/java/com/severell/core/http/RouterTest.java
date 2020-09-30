649a8101f88564d74ef76bf2ff5b7d562d0e734d
Beloved <belovedbb1@gmail.com>
2020-09-30 23:30:28 +0100

    null check for empty route

--- core/src/test/java/com/severell/core/http/RouterTest.java
+++ core/src/test/java/com/severell/core/http/RouterTest.java
@@ -2,6 +2,7 @@ package com.severell.core.http;
 
 import com.severell.core.container.Container;
 import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
@@ -26,6 +27,34 @@ public class RouterTest {
     private static Container c;
     private static ServletContextHandler context;
     private static ArrayList<RouteExecutor> routes = new ArrayList<>();
+
+    @BeforeAll
+    public static void routerReturnsNullForEmptyRoute() throws Exception {
+        Router.clearRoutes();
+        req = mock(Request.class);
+        context = mock(ServletContextHandler.class);
+        c = mock(Container.class);
+        routes =  new ArrayList<>();
+        Router.setCompiledRoutes(routes);
+
+        String path = "/";
+
+        Router router = new Router();
+        router.compileRoutes();
+        RouteExecutor route = null;
+        route = router.lookup(path, "GET", req);
+        assertNull(route);
+        route = router.lookup(path, "POST", req);
+        assertNull(route);
+        route = router.lookup(path, "PUT", req);
+        assertNull(route);
+        route = router.lookup(path, "PATCH", req);
+        assertNull(route);
+        route = router.lookup(path, "DELETE", req);
+        assertNull(route);
+        route = router.lookup(path, "OPTION", req);
+        assertNull(route);
+    }
 
     @BeforeEach
     public void setUp() throws NoSuchMethodException, ClassNotFoundException {
